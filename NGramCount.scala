0a81da6d30581ae4d29d1e685c21d768309bc849
Matheus Antunes Vieira <53957869+Mathunes@users.noreply.github.com>
2021-09-23 15:31:13 -0300

    first commit

---
+++ NGramCount.scala
@@ -0,0 +1,32 @@
+package com.javadeveloperzone.spark.scala
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+
+object NGramCount {
+
+	def main(args: Array[String]) {
+
+        /*By default we are setting local so it will run locally with one thread 
+         *Specify: "local[2]" to run locally with 2 cores, OR 
+         *        "spark://master:7077" to run on a Spark standalone cluster */
+
+		
+		val sparkContext = new SparkContext("local","Spark NGramCount example using Scala",
+		System.getenv("SPARK_HOME"))
+		//val sparkContext = sc
+
+		/*Reading input from File*/
+		val input = sparkContext.textFile(args(2))
+
+		/*Creating RDD from lines on input file*/
+		val ngrams = input.flatMap(line => line.split(" ").sliding(args(0).toInt))
+
+		/*Performing mapping and reducing operation*/
+		val counts = ngrams.map(ngram => (ngram.mkString(" "), 1)).reduceByKey{case (x, y) => x + y}.filter{case (x, y) => y >= args(1).toInt}
+
+		/*Saving the result file to the location that we have specified as args[1]*/
+		counts.saveAsTextFile(args(3))
+
+	}
+}
